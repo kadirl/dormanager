821830df129cde2eab65a3041588e984d0838cba
Nariman <issayevnariman@gmail.com>
2023-06-29 23:30:01 +0600

    update offer_router

--- app/keyboards/offer_menu.py
+++ app/keyboards/offer_menu.py
@@ -1,23 +1,24 @@
 import types
 
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardMarkup, InlineKeyboardButton
-def reply_to_offer():
-    builder = InlineKeyboardButton()
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardMarkup, InlineKeyboardButton, InlineKeyboardBuilder
 
-    builder.add(types.InlineKeyboardButton(
+def reply_to_offer():
+    builder = InlineKeyboardBuilder()
+
+    builder.add(InlineKeyboardButton(
         text="{ACCEPT}",
         callback_data="accept")
     )
-    builder.add(types.InlineKeyboardButton(
+    builder.add(InlineKeyboardButton(
         text="{COUNTER OFFER}",
         callback_data="counter_offer")
     )
-    builder.add(types.InlineKeyboardButton(
+    builder.add(InlineKeyboardButton(
         text="{DECLINE}",
         callback_data="decline")
     )
 
-    builder.adjust(3)
+    builder.adjust(2)
 
     return builder.as_markup()
--- app/routers/offer_router.py
+++ app/routers/offer_router.py
@@ -1,5 +1,6 @@
 import bson
 from aiogram import Router, types, F
+from aiogram.filters import Text
 from aiogram.filters.command import Command, CommandObject
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
@@ -14,19 +15,29 @@ router = Router()
 
 
 @router.message(Command('offer'))
-async def create_notification(message: types.Message, state: FSMContext):
+async def create_offer(message: types.Message, state: FSMContext):
     await state.set_state(OfferState.offer)
-    await message.answer('{OFFER REQUEST}')
+    await message.answer(text='{OFFER REQUEST}')
 
 
 @router.message(OfferState.offer)
-async def send_notification(message: types.Message, state: FSMContext):
+async def send_offer(message: types.Message, state: FSMContext):
     text = message.text
     users = UserCollection.get_offers_notification_allowed_users()
 
     for user in users:
         if user.tg_id == str(message.from_user.id):
-            await bot.send_message('{OFFER SENT}')
-        await bot.send_message(user.chat_id, text=text, reply_markup=offer_menu.reply_to_offer())
+            await message.answer('{OFFER SENT}')
+            continue
+        await bot.send_message(chat_id=user.chat_id, text=text, reply_markup=offer_menu.reply_to_offer())
 
-    await clear_history(state)
+    # await clear_history(state)
+
+
+@router.callback_query(Text("counter_offer"))
+async def send_random_value(callback: types.CallbackQuery):
+    await state.set_state(OfferState.counter_offer)
+
+@router.message(OfferState.counter_offer)
+async def send_counter_offer():
+    pass
